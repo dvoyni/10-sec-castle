c40299e8b3334a085c1c33cf3a0d79b6e7a2be5d
Cmaujluk <cmaujluk18@gmail.com>
2022-10-02 08:03:01 +0500

    Character view anim dead fix

--- Assets/Src/TenSecCastle.View/UnitView.cs
+++ Assets/Src/TenSecCastle.View/UnitView.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Rondo.Core;
 using Rondo.Unity.Components;
 using TenSecCastle.Model;
@@ -15,40 +16,40 @@ namespace TenSecCastle.View {
         private UnitState _currentState;
         private float _currentUnitHP;
         private float _currentAnimationProgress;
+        
+        private float _deadAnimationTime;
 
         public IMessenger Messenger { private get; set; }
 
-        public void Sync(Unit unit) {
+        private void Awake() {
+            Init();
+        }
+
+      public void Sync(Unit unit) {
             if (unit.State != _currentState) {
                 _currentState = unit.State;
                 _currentAnimationClip = GetAnimationClipName(_currentState);
             }
 
-            _currentProgress = unit.StateProgress;
+            _currentProgress = _currentState != UnitState.Dieing
+                ? unit.StateProgress
+                : Math.Clamp(unit.StateTime, 0, _deadAnimationTime);
 
             if (!Equals(string.IsNullOrEmpty(_currentAnimationClip))) {
                 _playerAnimator.speed = 0f;
                 _playerAnimator.Play(_currentAnimationClip, 0, _currentProgress);
             }
-
-            /*if (_currentProgress != unit.StateProgress && _currentState == UnitState.Attacking) {
-                if (_currentProgress > unit.StateProgress) {
-                    _playerAnimator.Play($"Male Attack {Random.Range(1, 4)}");
-                }
-                _currentProgress = unit.StateProgress;
-            }
-
-            if (unit.HitPoints != _currentUnitHP) {
-                _currentUnitHP = unit.HitPoints;
-                if (_currentUnitHP < unit.MaxHitPoints * 0.25f) {
-                    _playerAnimator.Play($"Male Sword Damage Heavy");
-                }
-                else {
-                    _playerAnimator.Play($"Male Sword Damage Light");
-                }
-            }*/
-
-            //animation.time = math.clamp(unit.StateTime, 0 , animation.length); 
+        }
+      
+        private void Init() {
+            
+            var animController = _playerAnimator.runtimeAnimatorController;
+            var animaName = GetAnimationClipName(UnitState.Dieing);
+            var clip = animController.animationClips.First(a => a.name == animaName);
+            _deadAnimationTime = clip.length;
+            
+            if (clip == null)
+                throw new Exception("Can't find animation");
         }
 
         private string GetAnimationClipName(UnitState unitState) {
