7c92f4aae4a3e89385cd6132fe6ab166142f8466
Cmaujluk <cmaujluk18@gmail.com>
2022-10-03 13:10:55 +0500

    a lot of things

---
+++ Assets/SPPatch/Editor/ExtendedHotkeys.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ExtendedHotkeys : ScriptableObject
+{
+    [MenuItem("MPUSP/Set Anchors %_q")]
+    static void SetAnchors()
+    {
+        var selectionObjects = Selection.gameObjects;
+        foreach (var t in selectionObjects)
+        {
+            if (t.GetComponent<RectTransform>())
+            {
+                var aspect = t.GetComponent<AspectRatioFitter>();
+                bool f = aspect != null;
+
+                if (t.GetComponent<AnchorTool>())
+                {
+                    DestroyImmediate(t.GetComponent<AnchorTool>());
+                }
+
+                if (f)
+                {
+                    aspect.enabled = false;
+                }
+                t.AddComponent(typeof(AnchorTool));
+                t.GetComponent<AnchorTool>().status = "Del";
+
+
+                Debug.Log("Anchors set for\"" + t.name + "\"");
+            }
+            else { Debug.Log("\"" + t.name + "\" have no anchors"); }
+        }
+    }
+}
